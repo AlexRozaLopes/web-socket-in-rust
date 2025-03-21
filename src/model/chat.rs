05ec6374e5635e129cfcdca63344a11d998a00be
AlexRozaLopes <alexrozaestudos@gmail.com>
2025-03-21 11:33:11 -0300

    sql fn

--- src/model/chat.rs
+++ src/model/chat.rs
@@ -1,8 +1,13 @@
 use crate::model::user::Usuario;
 use crate::schema::chat::dsl::chat;
 use crate::schema::chat::nome as chat_nome;
+use crate::schema::usuario::dsl::usuario;
 use crate::util::diesel_facade::establish_connection;
-use diesel::{Associations, Identifiable, QueryDsl, QueryResult, Queryable, QueryableByName, RunQueryDsl, Selectable, SelectableHelper,ExpressionMethods};
+use diesel::{
+    Associations, BelongingToDsl, ExpressionMethods, Identifiable, QueryDsl, QueryResult,
+    Queryable, QueryableByName, RunQueryDsl, Selectable, SelectableHelper,
+};
+use diesel::associations::HasTable;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -40,4 +45,13 @@ pub fn find_chat_by_name(name: &str) -> QueryResult<Chat> {
     chat.filter(chat_nome.eq(name))
         .select(Chat::as_select())
         .get_result(&mut connection)
+}
+
+pub fn find_user_in_chat(chat_name: &str) -> QueryResult<Vec<Usuario>> {
+    let mut connection = establish_connection();
+    let chat_by_name = find_chat_by_name(chat_name)?;
+    UsuarioChat::belonging_to(&chat_by_name)
+        .inner_join(usuario::table())
+        .select(Usuario::as_select())
+        .load(&mut connection)
 }
